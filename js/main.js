001f47fa62d67b13def337f3d1e283f1a123bd50
georgechan32 <georgechan32@gmail.com>
2015-05-27 00:12:17 -0700

    Added automatic scheduling based on priorities
    Lot of bugs, but if you ignore time and duration then it should work

--- js/main.js
+++ js/main.js
@@ -75,14 +75,19 @@ function toggle3()
 //e_name, e_time, e_date, e_duration, e_location, e_priority
 function realEvent()
 {
- 	
+	//alert($("#slider-range-max").slider( "option", "value" ));
+	clear_eventList();
+	var new_id =generateNodeId();
+ 	ll.scheduleAdd(document.getElementById("event_tt").value, document.getElementById("start_time").value,
+    	document.getElementById("event_date").value, document.getElementById("duration").value,
+    	document.getElementById("event_location").value, $("#slider-range-max").slider( "option", "value" ), new_id);
  	//alert($.cookie("events"));   
-    $('#schedule_list').append('<li id="add_schedule" class="list-group-item"><span><strong>' 
-            + document.getElementById("start_time").value + '</strong> ' + 
-            document.getElementById("event_tt").value + '</span><span class = "hit_right"><button type="button" class="btn btn-link" onclick = "cleanEvent('+document.getElementById("start_time").value+')">Delete</button></span></li>');
-    ll.add(document.getElementById("event_tt").value, document.getElementById("start_time").value
-    	, document.getElementById("event_date").value, document.getElementById("duration").value,
-    	document.getElementById("event_location").value, document.getElementById("priority").value)
+ 	var currNode = ll.head;
+ 	while(currNode)
+ 	{
+ 		add_cookie_event(currNode.name,currNode.s_time, currNode.id);
+ 		currNode = currNode.next;
+ 	}
     compile_events();
     document.getElementById("event_name").innerHTML = ll.head.name;
 
@@ -121,21 +126,18 @@ function fill_user()
 	if($.cookie("events") == null)
 	{
 		$.cookie("events", "");
-		//alert("Saving to cookies");
 
 	}
 	document.getElementById("user_name_goes_here").innerHTML = $.cookie("username");
 	if($.cookie("events") != "")
 	{
-		//alert($.cookie("events"));
 		var cookie_str = $.cookie("events");
 		var events = cookie_str.split("?");
 		for(var i = 0; i < events.length-1; i++)
 		{
 			var event_str = events[i].split("/");
-			add_cookie_event(event_str[1],event_str[2]);
-			ll.add(event_str[1],event_str[2],event_str[3],event_str[4],event_str[5]);
-			//fill_tasks(event_str[1], event_str[2]);
+			add_cookie_event(event_str[1],event_str[2], event_str[7]);
+			ll.add(event_str[1],event_str[2],event_str[3],event_str[4],event_str[5], event_str[6], event_str[7]);
 		}
 	}
 }
@@ -185,6 +187,12 @@ function add_eventer()
 	// }
 }
 
+function clear_eventList()
+{
+	var listParent = document.getElementById("schedule_list");
+	$('#schedule_list').empty();
+}
+
 function append_event()
 {
 	/*if(document.getElementById("event_tt").value != "" && document.getElementById("start_time").value != "")
@@ -202,10 +210,10 @@ function append_event()
 	}*/
 }
 
-function add_cookie_event(nodeName, nodeTime)
+function add_cookie_event(nodeName, nodeTime, nodeId)
 {
 	$('#schedule_list').append('<li id="'+nodeName+'" class="list-group-item"><span onclick = "add_eventer()"><strong>' 
-            + nodeTime + '</strong> ' + nodeName + '</span><span class = "hit_right"><button type="button" class="btn btn-link" onclick = "cleanEvent('+document.getElementById("start_time").value+')">Delete</button></span></li>');
+            + nodeTime + '</strong> ' + nodeName + '</span><span class = "hit_right"><button type="button" class="btn btn-link" onclick = "cleanEvent('+this.id+')">Delete</button></span></li>');
 	var fill_content = $(".list-group-item:first").text();
 	document.getElementById("event_name").innerHTML = fill_content.substr(0, fill_content.length-6);
 }
@@ -213,7 +221,7 @@ function add_cookie_event(nodeName, nodeTime)
 function cleanEvent(nodeName)
 {
 	//alert(nodeName.id);
-	ll.remove_param(nodeName.id);
+	ll.remove_param(nodeName);
 	compile_events();
 	//alert($.cookie("events"));
 	location.reload(); 
@@ -230,13 +238,13 @@ function compile_events()
 
 	while(ll_pointer != null)
 	{
-		string_cookie += "event/"+ll_pointer.name+"/"+ll_pointer.s_time+"/"+ll_pointer.date+"/"+ll_pointer.location+"/"+ll_pointer.priority+"?";
+		string_cookie += "event/"+ll_pointer.name+"/"+ll_pointer.s_time+"/"+ll_pointer.duration+"/"+ll_pointer.date+"/"+ll_pointer.location+"/"+ll_pointer.priority+"/"+ll_pointer.id+"?";
 		ll_pointer = ll_pointer.next;
 	}
 	//alert(string_cookie);
 	$.cookie("events",string_cookie);
+	//alert(string_cookie);
 }
-
 function cancel_add_event()
 {
 	add_eventer();
@@ -265,14 +273,49 @@ var makeLinkedList = function()
 
 var methodsOfLinkedList = 
 {
-	add: function(v1,v2,v3, v4, v5, v6)
+	add: function(v1,v2,v3, v4, v5, v6, v7)
 	{
-		var newNode = makeNode(v1,v2,v3, v4, v5, v6);
+		var newNode = makeNode(v1,v2,v3, v4, v5, v6,v7);
 		if(!this.head)
 			this.head = newNode;
 		if(this.tail)
 			this.tail.next = newNode;
 		this.tail = newNode;
+	},
+	scheduleAdd: function(v1,v2,v3, v4, v5, v6, v7)
+	{
+		//gonna disregard the tail node
+		var newNode = makeNode(v1,v2,v3, v4, v5, v6,v7);
+		if(this.head == null)
+		{
+			this.head = newNode;
+			//this.tail = newNode;
+			return;
+		}
+		if(this.head.priority < newNode.priority)
+		{
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		else
+		{
+			for(var iter = this.head; iter != null; iter = iter.next)
+			{
+				if(iter.next == null )
+				{
+					iter.next = newNode;
+					break;
+				}
+				else if(iter.priority > newNode.priority && iter.next.priority < newNode.priority)
+				{
+					newNode.next = iter.next;
+					iter.next = newNode;
+					location.reload();
+					break;
+				}
+				
+			}
+		}
 	},
 	remove_head: function()
 	{
@@ -317,21 +360,18 @@ var methodsOfLinkedList =
 			}
 		}
 	},
-	remove_param: function(nName)
+	remove_param: function(nodeId)
 	{
 		var node = this.head;
 		while(node)
 		{
-			//alert(node.name + " " + nName);
-			if(node.name == nName)
+			if(node.id == nodeId)
 			{
-			//	alert("FOUND");
 				this.remove(node);
 				return;
 			}
 			node = node.next;
 		}
-		//alert("ERROR< COULD NOT FIND?");
 	},
 	contains: function(value)
 	{
@@ -350,26 +390,42 @@ var methodsOfLinkedList =
 		var currentNode = this.head;
 		while(currentNode)
 		{
-			str += currentNode.data;
+			str += currentNode.name + " " +currentNode.priority + " " + currentNode.id;
 			if(currentNode = currentNode.next)
-				str += " -> ";
+				str += "\n";
 		}
 		console.log(str);
 		return str;
 	}
 };
 
-var makeNode = function(e_name, e_time, e_date, e_duration, e_location, e_priority)
+var makeNode = function(e_name, e_time, e_duration, e_date, e_location,e_priority, e_id)
 {
-	//alert("E TIME " + e_time);
-	var instanceOfNode = {
+	var instanceOfNode = 
+	{
 		name: e_name,
 		s_time: e_time,
 		duration: e_duration,
 		date: e_date,
 		location: e_location,
 		priority: e_priority,
+		id: e_id,
 		next: null
 	};
 	return instanceOfNode;
+}
+
+function generateNodeId()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for(var i=0; i < 6; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function printLinkedList()
+{
+	ll.print();
 }
